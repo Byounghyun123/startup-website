2598b63444ba68d784534e66a87325761e6dab2c
Byounghyun123 <kevinlee.number10@gmail.com>
2024-07-23 15:39:58 -0400

    collectionPage (i.e. Banner, collectionProfile, NFTCardTwo) added

---
+++ nft_frontend/collectionPage/collectionIndex.js
@@ -0,0 +1,9 @@
+import CollectionProfile from './collectionProfile/collectionProfile';
+import Banner from './Banner/Banner';
+import NFTCardTwo from './NFTCardTwo/NFTCardTwo';
+
+export {
+    CollectionProfile,
+    Banner,
+    NFTCardTwo
+}
